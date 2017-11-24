e82b31850248e64e2fb66618d1a63af0a2741df1
Hyun Heo <jheo2001@Jeffui-MacBook-Air.local>
2017-11-24 15:11:05 -0500

    Added 'Climbing Stairs' solution; also added gitignore so IDEA files are not added.

---
+++ 70-climbing-stairs/solution.py
@@ -0,0 +1,26 @@
+"""
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Problem and description courtesy of LeetCode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+:type n: int
+:rtype: int
+"""
+def climbStairs(self, steps):
+    if steps <= 2:
+        return steps
+
+    a = 1 # Step that is 2 steps before current step
+    b = 2 # Step that is 1 step before current step
+    c = 0 # current step
+
+    for i in range(3, steps+1):
+        c = a + b
+        a = b
+        b = c
+
+    return c
