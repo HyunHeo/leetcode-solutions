316c27abaed7bc910b6b9f26c3b240f5c0d0d191
Hyun Heo <hyunheosemi@gmail.com>
2017-11-24 20:39:46 -0500

    Fixed 'Climbing Stairs' solution's decription URL.

--- 70-climbing-stairs/solution.py
+++ 70-climbing-stairs/solution.py
@@ -5,7 +5,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 Note: Given n will be a positive integer.
 
-Problem and description courtesy of LeetCode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+Problem and description courtesy of LeetCode: https://leetcode.com/problems/climbing-stairs/description/
 
 :type n: int
 :rtype: int
